f2019795f220120d23a228e216c576976e89b644
AJ Leonard <axlgh45@gmail.com>
2018-12-14 09:36:44 -0500

    feat(ISchemaRegistryClient): Defined external module interface

---
+++ src/serialization/index.ts
@@ -0,0 +1,5 @@
+export * from './AvroSerializer';
+export * from './IMessageEncoder';
+export * from './ISchemaRegistryEncoding';
+export * from './ISerializer';
+export * from './SchemaRegistryEncoder';
